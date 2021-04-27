60f36a9b97dc8c2c34e55a2f174bc837e83b286c
Aimen <aimenhamed@live.com.au>
2021-04-27 18:57:25 +1000

    added functions to fish config

--- .config/fish/conf.d/omf.fish
+++ .config/fish/conf.d/omf.fish
@@ -3,14 +3,35 @@ set -q XDG_DATA_HOME
   and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
   or set -gx OMF_PATH "$HOME/.local/share/omf"
 
-# Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
-
 ### EXPORT ###
 set fish_greeting                                 # Supresses fish's intro message
-set TERM "xterm-256color"                         # Sets the terminal type
 
-### FUNCTIONS ###
+### Aliases ###
+
+alias vim=nvim
+
+# navigation
+#
+alias ...='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
+
+# git
+alias addup='git add -u'
+alias addall='git add .'
+alias branch='git branch'
+alias checkout='git checkout'
+alias clone='git clone'
+alias commit='git commit -m'
+alias fetch='git fetch'
+alias pull='git pull origin'
+alias push='git push origin'
+alias stat='git status'  # 'status' is protected name so using 'stat' instead
+alias tag='git tag'
+alias newtag='git tag -a'
+
+### Functions ###
 
 function mkcd
   mkdir -p $argv[1]
@@ -23,12 +44,32 @@ end
 
 function runsh
   chmod 755 *.sh
-  echo "Made .sh files executable."
+  if test $status -eq 0
+    echo -e Shell scripts now executable
+  else
+    echo ""
+    echo "No shell scripts found."
+  end
 end
 
 function runpl
   chmod 755 *.pl
-  echo "Made .pl files executable"
+  if test $status -eq 0
+    echo -e Perl scripts now executable
+  else
+    echo ""
+    echo "No perl scripts found."
+  end
+end
+
+function runfish
+  chmod 755 *.fish
+  if test $status -eq 0
+    echo -e Fish scripts now executable
+  else
+    echo ""
+    echo "No fish scripts found."
+  end
 end
 
 # Functions needed for !! and !$
@@ -40,6 +81,7 @@ function __history_previous_command
     commandline -i !
   end
 end
+
 function __history_previous_command_arguments
   switch (commandline -t)
   case "!"
@@ -58,22 +100,5 @@ else
   bind '$' __history_previous_command_arguments
 end
 
-### ALIASES ###
-
-#navigation
-alias ...='cd ../..'
-alias .3='cd ../../..'
-alias .4='cd ../../../..'
-alias .5='cd ../../../../..'
-
-# git
-alias addup='git add -u'
-alias addall='git add .'
-alias branch='git branch'
-alias checkout='git checkout'
-alias clone='git clone'
-alias commit='git commit -m'
-alias fetch='git fetch'
-alias pull='git pull origin'
-alias push='git push origin'
-alias stat='git status'  # 'status' is protected name so using 'stat' instead
+# Load Oh My Fish configuration.
+source $OMF_PATH/init.fish
